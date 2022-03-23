4f8c386ef98bb1085687941e0893035ca2054684
Olubummo Faith <aaeon456engineer@gmail.com>
2022-03-23 15:47:19 +0100

    fourth commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors());
 
 
 // set home page
-app.get("/", (req, res)=> res.sendFile(pathToViews + "/index.html"));
+app.get("/", async (req, res)=> res.sendFile(pathToViews + "/index.html"));
 
 
 
